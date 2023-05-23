2fa5f4dc759242406fbda6429ebfb3428e92353b
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-05-22 22:04:46 -0400

    skeleton with files and packages

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    
+)
+
+CREATE TABLE role (
+    
+)
+
+CREATE TABLE employees (
+
+)
+
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+// Import mysql12
+const mysql = require('mysql12');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+// Create Employee
+app.post('/api/new-employee', ({ body }, res) => {
+    const sql = `INSERT INTO employees (employee_name)
+    VALUES (?)`;
+    const params = [body.movie_name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
